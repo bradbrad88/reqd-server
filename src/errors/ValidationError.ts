b4115133138ef48ab35fbfe2f7e6c2cd9b9a9833
Brad Teague <b_rad88@live.com>
2023-08-29 22:31:42 +0930

    Create ValidationError class

---
+++ src/errors/ValidationError.ts
@@ -0,0 +1,10 @@
+class ValidationError extends Error {
+  constructor(msg: string) {
+    super("Validation Error" + msg);
+  }
+  static MinimumLength(str: string, minLength: number) {
+    return new ValidationError(
+      `The name provided: '${str}' is too short. It must be at least ${minLength} characters`
+    );
+  }
+}
