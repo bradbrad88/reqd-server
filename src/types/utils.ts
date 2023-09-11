c14157366e858a9a24c5652454d454518a31428a
Brad Teague <b_rad88@live.com>
2023-09-12 09:16:40 +0930

    Update OmitMethods type utility to Json - make it omit underscored properties as well

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,4 +1,11 @@
-export type OmitMethods<T extends Object> = Pick<T, PropertieKeysOnly<T>>;
+export type Json<T extends Object> = Pick<T, NoUnderscoredProperties<PropertieKeysOnly<T>>>;
+
+type NoUnderscoredProperties<T extends string> = Exclude<T, Underscored<string>>;
+
 type PropertieKeysOnly<T extends Object> = {
-  [K in keyof T]: T[K] extends Function ? never : K;
+  [K in keyof T]: T[K] extends Function ? never : K extends string ? K : never;
 }[keyof T];
+
+type Underscored<Str extends string> = Str extends number ? never : `_${Str}`;
+
+export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
