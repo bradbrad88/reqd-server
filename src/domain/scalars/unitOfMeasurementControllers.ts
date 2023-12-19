1793bcd701886d881148b2c4c897dcf0d6cb4c26
Brad Teague <b_rad88@live.com>
2023-12-19 17:58:06 +1030

    Chore: File cleanup
    Remove old pattern of persistence and interactors. Remove ProductLocation class

--- src/domain/scalars/unitOfMeasurementControllers.ts
+++
@@ -1,7 +0,0 @@
-import client from "../../../config/db";
-import { Controller } from "../../types/IController";
-
-export const getUnitOfMeasurementController: Controller = async () => {
-  const res = await client.unitOfMeasurement.findMany({});
-  return res;
-};
--- src/domain/vendors/vendorInteractor.ts
+++
@@ -1,7 +0,0 @@
-import Vendor from "./Vendor";
-import type { VendorJson } from "./Vendor";
-
-export const createVendorInteractor = (newVendor: VendorJson) => {
-  const vendor = new Vendor(newVendor);
-  return vendor;
-};
