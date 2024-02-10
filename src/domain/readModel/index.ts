8484eb4455f159b1c0176d7309f108dc8c4b9b5a
Brad Teague <b_rad88@live.com>
2024-02-10 20:53:41 +1030

    Chore: Include order queries on readModel index file
    Also remove product queries that are no longer used

--- src/domain/readModel/index.ts
+++ src/domain/readModel/index.ts
@@ -1,5 +1,5 @@
-export * from "./productQueries";
 export * from "./areaQueries";
 export * from "./inventoryQueries";
 export * from "./enumerablesQueries";
 export * from "./preferredVendorQueries";
+export * from "./orderQueries";
