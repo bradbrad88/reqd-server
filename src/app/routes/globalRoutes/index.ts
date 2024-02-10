2e09c3fdde295a38478b4052e70dcfc741e24c5a
Brad Teague <b_rad88@live.com>
2024-02-10 20:33:34 +1030

    Chore: Remove global product controller and queries
    The main product query was still found to belong more in venue routes as it returned data on whether it belonged to the venue's inventory. Most functionality removed here now exists in Inventory queries and controllers

--- src/app/routes/globalRoutes/index.ts
+++ src/app/routes/globalRoutes/index.ts
@@ -1,14 +1,11 @@
 import { Router } from "express";
-import { getProductGlobalQueryControllers } from "./productGlobalControllers";
 import { getEnumerablesGlobalQueryControllers } from "./enumerables";
 import adaptor from "../../expressAdaptor";
 
 const router = Router({ mergeParams: true });
 
-const globalProductQueryRoutes = getProductGlobalQueryControllers(adaptor);
 const globalEnumerableQueryRoutes = getEnumerablesGlobalQueryControllers(adaptor);
 
-router.use("/products", globalProductQueryRoutes);
 router.use("/enumerables", globalEnumerableQueryRoutes);
 
 export default router;
